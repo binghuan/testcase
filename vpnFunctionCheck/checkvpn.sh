86ff75caf4de2a62ad08f8b857f2ef87d08d959e
BH_Lin <binghuanlin@gmail.com>
2014-09-30 18:15:26 +0800

    correct function to check van's functionality.

--- vpnFunctionCheck/checkvpn.sh
+++ vpnFunctionCheck/checkvpn.sh
@@ -5,10 +5,10 @@ echo "description: pre-check funtionality for test environment."
 #echo "#1 check network interface"
 #result=$(ifconfig | grep -i -E "utun0|utun1" );
 #if [ "${result}" != "" ]; then
+#    echo "OK: #1 check Network Interface for VPN"
+#else
 #    echo "FAIL: #1 check Network Interface for VPN"
 #    exit 0;
-#else
-#    echo "OK: #1 check Network Interface for VPN"
 #fi
 
 #result=$(curl "http://binghuan.github.io/debug/index.html" | grep -i -E "extensionFrame/content_script.js")
